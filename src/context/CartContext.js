8541af639d56fe7290dafb6a49b27e0ddf875c24
Akira T <akirajit@gmail.com>
2021-05-06 00:29:02 -0300

    working on useContext

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,2 +1,10 @@
-import React, { createContext } from "react";
-export const CartContext = React.createContext([]);
+import { createContext, useState } from "react";
+export const CartContext = createContext(null);
+
+export default function AppContextProvider({ children }) {
+  return (
+    <CartContext.Provider value={{ brand: "fender" }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
